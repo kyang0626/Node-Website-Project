4b3a0ae1fb31636d6c0dadaf2301d36fe7129a7c
Kong <kong_yang828@hotmail.com>
2019-10-30 14:38:39 -0400

    2nd commit

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 
 var port = process.env.PORT || 3000;
+
+// app.use(express.static("public"));
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var jsonParser = bodyParser.json();
 
+var sibling = require('./views/famMember.json');
+
 // ===============    Routes ===============
 app.use('/public', express.static(__dirname + '/public'));
+
 
 app.set('view engine', 'ejs');
 
@@ -28,25 +34,56 @@ app.get('/contact', function (req, res) {
     res.render('contact');
 })
 app.get('family/:id', function (req, res) {
-    res.render('person', { ID: req.params.id });
+    res.render('person', { id: req.params.id });
 })
-app.get('/api', function (req, res) {
-    req.json({ firstname: 'John', lastname: 'Doe' });
-})
+// get JSON by params 
+app.get('/family/:id', function (req, res) {
+    console.log(req);
+    res.json({ sibling: sibling.siblings.firstSibling });
+    res.json({ sibling: sibling.siblings.secondSibling });
+    res.json({ sibling: sibling.siblings.thirdSibling });
+
+});
+
+
 // app.post('/person', urlencodedParser, function (req, res) {
 //     console.log('hello!');
 //     res.send("Thank you!");
 //     console.log(req, res, firstname);
 //     console.log(req, res, lastname);
-// })
+// }
 
 app.post("/contact", urlencodedParser, function (req, res) {
     console.log(req.body.name);
     console.log(req.body.email);
+    console.log(req.body.comment);
+
+
+    // MySql Connection
+    var con = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'guestbook_db'
+    });
+
+    con.query('SELECT * FROM guestbook_db.people',
+        function (err, rows) {
+            if (err) {
+                res.status(500);
+                throw err
+            };
+        })
+
+
+    // Insert
+    con.query("INSERT INTO people (name, email, comment) VALUES ('" + req.body.name + "', '" + req.body.email + "', '" + req.body.comment + "')", function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
 
 })
 
-app.listen(port);
 
-// Logic
+app.listen(port);
 
