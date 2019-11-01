96a0dad27a4009fab126a2b5124e371d4147831a
Kong <kong_yang828@hotmail.com>
2019-11-01 16:01:01 -0400

    added controller

---
+++ controller/contact.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+router.get('/contact', function (req, res) {
+    // app.get('/contact', function (req, res) {
+    //     res.render('contact');
+    // })
+    // Submit Form
+    app.post("/contact", urlencodedParser, function (req, res) {
+        var timeSubmit = new Date().toLocaleTimeString();
+        app.use(fileUpload());
+
+        MongoClient.connect(uri, function (err, client) {
+            if (err) {
+                console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+            }
+            console.log('MongoDB sucessfully connected!!.....');
+            const collection = client.db("guestbook_db").collection("people");
+            // perform actions on the collection object
+            var doc = { name: req.body.name, email: req.body.email, comment: req.body.comment, time: timeSubmit };
+            collection.insertOne(doc, function (err, res) {
+                if (err) throw err;
+                console.log("Document inserted");
+                console.log(doc);
+                // close the connection to db when you are done with it
+                // db.close();
+            });
+            client.close();
+            // ======= File Upload ============
+            var imageFile = req.files.imageFile;
+            console.log(imageFile);
+            // Use the mv() method to place the file somewhere on your server
+            imageFile.mv('public/uploads/' + imageFile.name, function (err) {
+                if (err) {
+                    // return res.status(500).send(err);
+                    console.log('error');
+                    console.log("Couldn't upload the image file.");
+                } else {
+                    // res.send('File uploaded!');
+                    console.log("Image file uploaded successfully.");
+                }
+            });
+        });
+
+        res.redirect("success");
+
+    });
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,30 +1,14 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-// var mysql = require('mysql');
+const fileUpload = require('express-fileupload');
+var content = require('./content.json');
 
 var port = process.env.PORT || 3000;
-
-// app.use(express.static("public"));
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var jsonParser = bodyParser.json();
 
-var content = require('./content.json');
-
-const fileUpload = require('express-fileupload');
-
-app.use(fileUpload());
-
-// ******* No Longer Using MySql ************
-// MySql Connection
-// var con = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'root',
-//     database: 'guestbook_db'
-// });
-// *********************************************
 
 // ========== MongoDB CONNECTION ===================
 
@@ -41,7 +25,7 @@ MongoClient.connect(uri, function (err, client) {
     // perform actions on the collection object
     client.close();
 });
-
+//===================================================
 
 app.use('/public', express.static(__dirname + '/public'));
 app.set('view engine', 'ejs');
@@ -49,6 +33,10 @@ app.set('view engine', 'ejs');
 app.use(fileUpload());
 
 // ===============    Routes ===============
+// =====  contact page ======
+var contactController = require('./controller/contact.js');
+app.use('/contact', contactController);
+
 // render pages
 app.get('/', function (req, res) {
     res.render('index');
@@ -59,9 +47,9 @@ app.get('/aboutme', function (req, res) {
 app.get('/family', function (req, res) {
     res.render('family');
 })
-app.get('/contact', function (req, res) {
-    res.render('contact');
-})
+
+
+
 app.get('/contact', function (req, res) {
     res.render('contact');
 })
@@ -75,9 +63,8 @@ app.get('/family/:id', function (req, res) {
 app.get('/guestbook', function (req, res) {
     var resultArray = [];
     const MongoClient = require('mongodb').MongoClient;
-
-    // replace the uri string with your connection string.
     const uri = "mongodb+srv://yangk93:koobyaj828@cluster0-w1hli.mongodb.net/test?retryWrites=true&w=majority"
+
     MongoClient.connect(uri, function (err, client) {
         if (err) {
             console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
@@ -109,49 +96,5 @@ app.get('/api/content/:id', function (req, res) {
     res.json(req.params.id)
 
 })
-
-// Submit Form
-app.post("/contact", urlencodedParser, function (req, res) {
-    var timeSubmit = new Date().toLocaleTimeString();
-
-    // ======= File Upload ============
-    var imageFile = req.files.imageFile;
-    // Use the mv() method to place the file somewhere on your server
-    imageFile.mv('public/uploads/' + imageFile.name, function (err) {
-        if (err) {
-            // return res.status(500).send(err);
-            console.log('error');
-            console.log("Couldn't upload the image file.");
-        } else {
-            // res.send('File uploaded!');
-            console.log("Image file uploaded successfully.");
-        }
-    });
-
-    MongoClient.connect(uri, function (err, client) {
-        if (err) {
-            console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
-        }
-        console.log('MongoDB sucessfully connected!!.....');
-        const collection = client.db("guestbook_db").collection("people");
-        // perform actions on the collection object
-        var doc = { name: req.body.name, email: req.body.email, comment: req.body.comment, time: timeSubmit };
-        collection.insertOne(doc, function (err, res) {
-            if (err) throw err;
-            console.log("Document inserted");
-            console.log(doc);
-            // close the connection to db when you are done with it
-            // db.close();
-        });
-        client.close();
-    });
-
-
-    res.redirect("success");
-
-});
-
-// mongoconnection = mongodb+srv://yangk93:<password>@cluster0-w1hli.mongodb.net/test?retryWrites=true&w=majority
-
 
 app.listen(port);
